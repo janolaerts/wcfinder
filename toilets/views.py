3f5ec4b765ff7d6e34ee02e24d25086934b7d713
iamjanolaerts <jan.olaerts92@gmail.com>
2020-01-13 12:49:11 +0100

    Filtering cities works

--- toilets/views.py
+++ toilets/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from . models import Toilet
 
 # Create your views here.
+def filter_cities(toilet):
+  return toilet.city
+
 def wclist_view(request):
   toilets = Toilet.objects.all()
-  return render(request, 'toilets/wc_list.html', { 'toilets': toilets })
+  cities = list(map(filter_cities, toilets))
+  filtered_cities = list(set(cities))
+  return render(request, 'toilets/wc_list.html', { 'toilets': toilets, 'cities': filtered_cities })

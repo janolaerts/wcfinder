0ba900ca5b79cf5d06a8ae4138888e92a9fce57d
iamjanolaerts <jan.olaerts92@gmail.com>
2020-01-17 11:48:42 +0100

    Checkboxes work

--- toilets/forms.py
+++ toilets/forms.py
@@ -2,8 +2,8 @@ from django import forms
 from . import models
 
 class AddToilet(forms.ModelForm):
-  clean = forms.BooleanField(required = False, label = "Is the toilet clean?")
-  accessible = forms.BooleanField(required = False, label = "Is the toilet accessible for wheelchairs?")
+  #clean = forms.BooleanField(required = False, label = "Clean") 
+  #accessible = forms.BooleanField(required = False, label = "Wheelchair")
 
   class Meta:
     model = models.Toilet

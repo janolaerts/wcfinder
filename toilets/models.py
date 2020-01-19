9abc708a184065f6136fa7148c59e1e0098e6b76
iamjanolaerts <jan.olaerts92@gmail.com>
2020-01-19 15:15:18 +0100

    Solved shifted street label

--- toilets/models.py
+++ toilets/models.py
@@ -2,7 +2,30 @@ from django.db import models
 
 # Create your models here.
 class Toilet(models.Model):
-  city = models.CharField(max_length = 20, default = None)
+  CITIES_CHOICES = (
+    ('Antwerp', 'Antwerp'),
+    ('Bruges', 'Bruges'),
+    ('Brussels', 'Brussels'),
+    ('Charleroi', 'Charleroi'),
+    ('Ghent', 'Ghent'),
+    ('Hasselt', 'Hasselt'),
+    ('Kortrijk', 'Kortrijk'),
+    ('Leuven', 'Leuven'),
+    ('Liege', 'Liege'),
+    ('Mechelen', 'Mechelen'),
+    ('Mons', 'Mons'),
+    ('Namur', 'Namur'),
+    ('Nivelles', 'Nivelles'),
+    ('Ostend', 'Ostend'),
+    ('Tournai', 'Tournai'),
+  )
+
+  CLEAN_CHOICES = (
+    ('Yes', 'Yes'),
+    ('No', 'No')
+  )
+
+  city = models.CharField(max_length = 20, default = 'Antwerp', choices = CITIES_CHOICES)
   street = models.CharField(max_length = 50, default = None)
   number = models.IntegerField(default = None)
   reference = models.CharField(max_length = 20)
